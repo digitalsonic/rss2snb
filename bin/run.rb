e4d4125941642d238d7188dee3c1cb6475ef7a26
digitalsonic <digitalsonic@yahoo.cn>
2010-11-26 10:42:17 +0800

    modify run.rb, remove native codes

--- bin/run.rb
+++ bin/run.rb
@@ -3,17 +3,7 @@ require 'rubygems'
 gem 'rss2snb'
 require 'rss2snb'
 
-require 'java'
-require 'jna.jar'
-require 'BambookLib.jar'
-import com.sun.jna.Native
-import com.sdo.bambooksdk.BambookCoreJRubyHelper
 
-def pack_to_snb snb_file, snb_dir
-  suffix = Util::is_windows? ? "" : ".so"
-  Native.loadLibrary("#{File.expand_path(File.dirname(__FILE__))}/BambookCore#{suffix}".to_java, BambookCoreJRubyHelper.getBambookCoreClass()).BambookPackSnbFromDir(snb_file, snb_dir)
-  Logger.new(STDOUT).warn "Writing #{snb_file}. DONE!"
-end
 
 snb = Rss2Snb.new('config.yml').run
-pack_to_snb snb[:file], snb[:dir]
+#pack_to_snb snb[:file], snb[:dir]
