e4c0e88754a9c61ec0bf93fffd6701e00ba44327
DigitalSonic <digitalsonic.dxf@gmail.com>
2011-02-18 21:52:42 +0800

    fix broken myNoSQL plugin

--- lib/rss/plugin/my_no_sql.rb
+++ lib/rss/plugin/my_no_sql.rb
@@ -8,7 +8,7 @@ module Rss
     # Plugin to fetch content from http://nosql.mypopescu.com .
     class MyNoSql < Feedproxy
       def post_process content
-        Nokogiri::HTML(content).xpath('//div[contains(@class,"entrybody")]').first.to_html
+        Nokogiri::HTML(content).xpath('//div[contains(@class,"entry_body")]').first.to_html
       end
     end
   end
